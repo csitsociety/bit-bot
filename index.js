a9b905f002df998cb1e31611f4e2ea7b1cf76977
Benjamin Grant <benjamin.grantGRA0007@gmail.com>
2020-04-16 01:07:48 +1000

    Improve message readability

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ client.on('guildMemberAdd', member => {
 	if (!channel) return;
 	const introChannel = '415417332576288768';
 	let welcomes = [
-		`\`Beep boop, it is wonderful to see you-, yes, it's great to have you here \`${member}\`! Please, tell us a bit about yourself in \`<#${introChannel}>\` boop?\` ${emotes.csit}`,
-		`\`Beep boop, thank you- for joining our server! I am really glad you're here \`${member}\`, and don't forget to introduce yourself in \`<#${introChannel}>\` beep.\` ${emotes.csit}`,
-		`\`Beep boop, it's me, Bit! I'm your friendly guide here at the CSIT Society \`${member}\`. I'd love to get to know you better-, in \`<#${introChannel}>\`? boop.\` ${emotes.csit}`,
-		`\`Beeep boop, hiya \`${member}\` and welcome to our server! Please-, share a bit about yourself in \`<#${introChannel}>\` so we can get to know you, beep.\` ${emotes.csit}`
+		`\`Beep boop\`, it is wonderful to see you-, yes, it's great to have you here ${member}! Please, tell us a bit about yourself in <#${introChannel}> \`boop\`? ${emotes.csit}`,
+		`\`Beep boop\`, thank you- for joining our server! I am really glad you're here ${member}, and don't forget to introduce yourself in <#${introChannel}> \`beep\`. ${emotes.csit}`,
+		`\`Beep boop\`, it's me, Bit! I'm your friendly guide here at the CSIT Society ${member}. I'd love to get to know you better-, in <#${introChannel}>? \`boop\`. ${emotes.csit}`,
+		`\`Beeep boop\`, hiya ${member} and welcome to our server! Please-, share a bit about yourself in <#${introChannel}> so we can get to know you, \`beep\`. ${emotes.csit}`
 	];
 	channel.send(welcomes[Math.floor(Math.random() * welcomes.length)]);
 });
